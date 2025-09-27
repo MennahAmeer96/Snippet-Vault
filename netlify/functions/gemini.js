52482f2db6380980957e463ee6b9f0dcbccf30e5
MennahAmeer96 <mennah.ameer244@gmail.com>
2025-09-27 18:20:43 +0300

    Changed the AI model to gemini-2.5-pro

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -20,7 +20,7 @@ exports.handler = async function (event, context) {
         throw new Error('Invalid AI request type.');
     }
 
-    const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=' + apiKey, {
+    const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent?key=' + apiKey, {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json',
